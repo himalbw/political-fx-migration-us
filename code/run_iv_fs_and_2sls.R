33baff66da702167ba24fe4a8d8905f2a620c2e3
himalbw <himalb23@gmail.com>
2025-12-08 21:04:14 -0500

    Construct shift-share instruments overall, and for HS and LS with different splits; make table

---
+++ code/run_iv_fs_and_2sls.R
@@ -0,0 +1,147 @@
+# 0. Install any packages
+library(readr) 
+library(modelsummary)
+
+# 1. Load Data
+cy <- read_csv("./data/acs_05_23.csv")
+load("./data/base_shares_2000.RData")
+load("./data/nat_origin_flows_3y.RData")
+vote_df <- read_csv('./data/votes_by_county.csv')
+
+# 2. Construct Shift-Share
+bs   <- as.data.table(base_shares)        
+no   <- as.data.table(nat_origin)
+noA  <- as.data.table(nat_origin_skillA)
+noB  <- as.data.table(nat_origin_skillB)
+
+# 2a. Overall Instrument
+Z_all <- merge(bs, no[, .(origin, YEAR, g_3y)],  by = "origin", allow.cartesian = TRUE)
+Z_all[, Z_term := s_co * g_3y]
+Z_all <- Z_all %>% rename(county_fips = full_county_fips)
+Z_overall <- Z_all[, .(Z = sum(Z_term, na.rm = TRUE)), by = .(county_fips, YEAR)]
+
+# b. HS Instrument (a) 
+Z_A_dt <- merge(bs,noA[, .(origin, skill_group_a, YEAR, g_3y)],by = "origin", allow.cartesian = TRUE)
+Z_A_dt[, Z_term := s_co * g_3y]
+Z_A_dt <- Z_A_dt %>% rename(county_fips = full_county_fips)
+Z_A <- Z_A_dt[
+  ,
+  .(
+    Z_lowa  = sum(Z_term[skill_group_a == "low"],  na.rm = TRUE),
+    Z_higha = sum(Z_term[skill_group_a == "high"], na.rm = TRUE)
+  ),
+  by = .(county_fips, YEAR)
+]
+Z_B_dt <- merge(bs, noB[, .(origin, skill_group_b, YEAR, g_3y)], by = "origin", allow.cartesian = TRUE)
+Z_B_dt[, Z_term := s_co * g_3y]
+Z_B_dt <- Z_B_dt %>% rename(county_fips = full_county_fips)
+Z_B <- Z_B_dt[
+  ,
+  .(
+    Z_lowb  = sum(Z_term[skill_group_b == "low"],  na.rm = TRUE),
+    Z_highb = sum(Z_term[skill_group_b == "high"], na.rm = TRUE)
+  ),
+  by = .(county_fips, YEAR)
+]
+
+Z_panel <- Reduce(
+  function(d1, d2) merge(d1, d2, by = c("county_fips", "YEAR"), all = TRUE),
+  list(Z_overall, Z_A, Z_B)
+)
+save(Z_panel, file = "./data/shiftshare_Z_panel.RData")
+
+# prep cy for merge 
+make_cy_small <- function(cy) {
+  cy_small <- cy %>%
+    select(
+      county_fips, YEAR,
+      pop_total,
+      foreign_flow_3y,              # ✅ put this back in
+      fb_lt_hs_share, fb_hs_share,
+      fb_sc_share, fb_ba_share, fb_grad_share
+    ) %>%
+    arrange(county_fips, YEAR) %>%
+    group_by(county_fips) %>%
+    mutate(
+      # -------------------------------------------------------------
+      # 1. Convert shares to LEVELS (counts) for each skill component
+      # -------------------------------------------------------------
+      fb_lt_hs_count  = fb_lt_hs_share  * pop_total,
+      fb_hs_count     = fb_hs_share     * pop_total,
+      fb_sc_count     = fb_sc_share     * pop_total,
+      fb_ba_count     = fb_ba_share     * pop_total,
+      fb_grad_count   = fb_grad_share   * pop_total,
+      
+      # Cutoff A: low = lt_hs | high = hs+sc+ba+grad
+      fb_low_A_count  = fb_lt_hs_count,
+      fb_high_A_count = fb_hs_count + fb_sc_count + fb_ba_count + fb_grad_count,
+      
+      # Cutoff B: low = lt_hs + hs | high = sc+ba+grad
+      fb_low_B_count  = fb_lt_hs_count + fb_hs_count,
+      fb_high_B_count = fb_sc_count + fb_ba_count + fb_grad_count,
+      
+      # -------------------------------------------------------------
+      # 2. Compute 3-year lag in LEVELS
+      # -------------------------------------------------------------
+      fb_low_A_lag3   = lag(fb_low_A_count, 3),
+      fb_high_A_lag3  = lag(fb_high_A_count, 3),
+      
+      fb_low_B_lag3   = lag(fb_low_B_count, 3),
+      fb_high_B_lag3  = lag(fb_high_B_count, 3),
+      
+      # -------------------------------------------------------------
+      # 3. Compute 3-year flows in LEVELS
+      # -------------------------------------------------------------
+      d_fb_low_skill_a_3y  = fb_low_A_count  - fb_low_A_lag3,
+      d_fb_high_skill_a_3y = fb_high_A_count - fb_high_A_lag3,
+      
+      d_fb_low_skill_b_3y  = fb_low_B_count  - fb_low_B_lag3,
+      d_fb_high_skill_b_3y = fb_high_B_count - fb_high_B_lag3
+    ) %>%
+    ungroup()
+  
+}
+
+cy_small <- make_cy_small(cy)
+cy_small  <- cy_small  %>% mutate(county_fips = as.integer(county_fips))
+Z_panel   <- Z_panel   %>% mutate(county_fips = as.integer(county_fips))
+panel_fs <- cy_small %>% left_join(Z_panel, by = c("county_fips", "YEAR"))
+
+# Overall inflow
+fs_overall <- feols(
+  foreign_flow_3y ~ Z | county_fips + YEAR,
+  data    = panel_fs,
+  cluster = ~county_fips
+)
+summary(fs_overall)
+
+# Cutoff A: low = lt_hs, high = hs+sc+ba+grad
+fs_low_A <- feols(
+  d_fb_low_skill_a_3y ~ Z_lowa | county_fips + YEAR,
+  data    = panel_fs,
+  cluster = ~county_fips
+)
+summary(fs_low_A)
+
+fs_high_A <- feols(
+  d_fb_high_skill_a_3y ~ Z_higha | county_fips + YEAR,
+  data    = panel_fs,
+  cluster = ~county_fips
+)
+summary(fs_high_A)
+
+# Cutoff B: low = lt_hs + hs, high = sc+ba+grad
+fs_low_B <- feols(
+  d_fb_low_skill_b_3y ~ Z_lowb  | county_fips + YEAR,
+  data    = panel_fs,
+  cluster = ~county_fips
+)
+summary(fs_low_B)
+
+fs_high_B <- feols(
+  d_fb_high_skill_b_3y ~ Z_highb | county_fips + YEAR,
+  data    = panel_fs,
+  cluster = ~county_fips
+)
+summary(fs_high_B)
+
