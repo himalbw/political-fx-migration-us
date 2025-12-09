45123fc0ae9629944ce82f5fccc53878055ce47f
himalbw <himalb23@gmail.com>
2025-12-09 07:35:57 -0500

    fix 24 election year for panel and save

--- code/run_iv_fs_and_2sls.R
+++ code/run_iv_fs_and_2sls.R
@@ -106,8 +106,13 @@ make_cy_small <- function(cy) {
 
 cy_small <- make_cy_small(cy)
 cy_small  <- cy_small  %>% mutate(county_fips = as.integer(county_fips))
+cy_small <- cy_small %>% mutate(election_year = ifelse(YEAR < 2023, YEAR, 2024))
+
 Z_panel   <- Z_panel   %>% mutate(county_fips = as.integer(county_fips))
-panel_fs <- cy_small %>% left_join(Z_panel, by = c("county_fips", "YEAR"))
+Z_panel <- Z_panel %>% mutate(election_year = ifelse(YEAR < 2023, YEAR, 2024))
+
+panel_fs <- cy_small %>% left_join(Z_panel, by = c("county_fips", "election_year"))
+panel_fs$YEAR <- panel_fs$election_year
 
 # Overall inflow
 fs_overall <- feols(
@@ -156,6 +161,90 @@ first_stage_models <- list(
   "High-skill inflow (B)"     = fs_high_B
 )
 
+coef_map <- c(
+  "Z"       = "Overall instrument Z",
+  "Z_lowa"  = "Low-skill instrument (A): Z_lowa",
+  "Z_higha" = "High-skill instrument (A): Z_higha",
+  "Z_lowb"  = "Low-skill instrument (B): Z_lowb",
+  "Z_highb" = "High-skill instrument (B): Z_highb"
+)
+
+modelsummary(
+  first_stage_models,
+  coef_map  = coef_map,
+  coef_omit = "Intercept",                 # hide intercept if present
+  stars     = TRUE,
+  #fmt       = sci_fmt,
+  gof_omit  = "IC|Log|Adj|Pseudo",
+  output    = "./output/tables/first_stage_results_with24.html",
+  title     = "First Stage Regressions: Shift-Share for 3-Year Inflows"
+)
+
+# Overall inflow
+fs_overall_e <- feols(
+  foreign_flow_3y ~ Z | county_fips + YEAR,
+  data    = election_panel_iv,
+  cluster = ~county_fips
+)
+
+# Cutoff A: low = lt_hs, high = hs+sc+ba+grad
+fs_low_A_e <- feols(
+  d_fb_low_skill_a_3y ~ Z_lowa | county_fips + YEAR,
+  data    = election_panel_iv,
+  cluster = ~county_fips
+)
+
+fs_high_A_e <- feols(
+  d_fb_high_skill_a_3y ~ Z_higha | county_fips + YEAR,
+  data    = election_panel_iv,
+  cluster = ~county_fips
+)
+
+# Cutoff B: low = lt_hs + hs, high = sc+ba+grad
+fs_low_B_e <- feols(
+  d_fb_low_skill_b_3y ~ Z_lowb | county_fips + YEAR,
+  data    = election_panel_iv,
+  cluster = ~county_fips
+)
+
+fs_high_B_e <- feols(
+  d_fb_high_skill_b_3y ~ Z_highb | county_fips + YEAR,
+  data    = election_panel_iv,
+  cluster = ~county_fips
+)
+
+## 2. Put in a modelsummary table -------------------------------------
+
+dir.create("./output/tables", recursive = TRUE, showWarnings = FALSE)
+
+first_stage_models_e <- list(
+  "Overall inflow"            = fs_overall_e,
+  "Low-skill inflow (A)"      = fs_low_A_e,
+  "High-skill inflow (A)"     = fs_high_A_e,
+  "Low-skill inflow (B)"      = fs_low_B_e,
+  "High-skill inflow (B)"     = fs_high_B_e
+)
+
+coef_map <- c(
+  "Z"       = "Overall instrument Z",
+  "Z_lowa"  = "Low-skill instrument (A): Z_lowa",
+  "Z_higha" = "High-skill instrument (A): Z_higha",
+  "Z_lowb"  = "Low-skill instrument (B): Z_lowb",
+  "Z_highb" = "High-skill instrument (B): Z_highb"
+)
+
+modelsummary(
+  first_stage_models_e,
+  coef_map  = coef_map,
+  coef_omit = "Intercept",
+  stars     = TRUE,
+  # fmt     = sci_fmt,  # uncomment if you want scientific notation
+  gof_omit  = "IC|Log|Adj|Pseudo",
+  output    = "./output/tables/first_stage_results_election_years.html",
+  title     = "First Stage Regressions (Election Years Only)"
+)
+
+
 ## SECOND STAGE
 
 vote_formatted <- vote_df %>%
@@ -191,8 +280,8 @@ panel_iv <- panel_fs %>%
   # keep only presidential election years where we actually observe votes
   filter(!is.na(gop_two_party_share))
 
-dem_controls <- cy %>% select(county_fips, YEAR, hispanic_native_share, white_share, black_share, native_lt_hs_share, native_hs_share)
-panel_iv <- panel_iv %>% left_join(dem_controls, by = c("county_fips", "YEAR"))
+dem_controls <- cy %>% mutate(election_year = ifelse(YEAR < 2023, YEAR, 2024)) %>% select(county_fips, election_year, hispanic_native_share, white_share, black_share, native_lt_hs_share, native_hs_share)
+panel_iv <- panel_iv %>% left_join(dem_controls, by = c("county_fips", "YEAR" = "election_year"))
 
 iv_total <- feols(
   gop_two_party_share ~ gop_two_party_share_lag4 + pop_total + hispanic_native_share + white_share + native_lt_hs_share |
@@ -201,6 +290,14 @@ iv_total <- feols(
   data    = panel_iv,
   cluster = ~county_fips
 )
+iv_low_A <- feols(
+  gop_two_party_share ~ gop_two_party_share_lag4 + pop_total + hispanic_native_share + white_share + native_lt_hs_share |
+    county_fips + YEAR |
+    d_fb_low_skill_a_3y ~ Z_lowa,
+  data    = panel_iv,
+  cluster = ~county_fips
+)
+
 
 iv_high_A <- feols(
   gop_two_party_share ~ gop_two_party_share_lag4 + pop_total + hispanic_native_share + white_share + native_lt_hs_share |
@@ -226,5 +323,84 @@ iv_high_B <- feols(
   cluster = ~county_fips
 )
 
+
+two_sls_models <- list(
+  "Total inflow (IV)"         = iv_total,
+  "High-skill inflow A (IV)"  = iv_high_A,
+  "Low-skill inflow A (IV)"   = iv_low_A,
+  "Low-skill inflow B (IV)"   = iv_low_B,
+  "High-skill inflow B (IV)"  = iv_high_B
+)
+
+coef_map <- c(
+  # fitted IV coefficients
+  "fit_foreign_flow_3y"       = "Total foreign inflow (3y)",
+  "fit_d_fb_high_skill_a_3y"  = "High-skill inflow, A (3y)",
+  "fit_d_fb_low_skill_a_3y"   = "Low-skill inflow, A (3y)",
+  "fit_d_fb_low_skill_b_3y"   = "Low-skill inflow, B (3y)",
+  "fit_d_fb_high_skill_b_3y"  = "High-skill inflow, B (3y)",
+  
+  # controls / lag
+  "gop_two_party_share_lag4"  = "GOP share, last election",
+  "pop_total"                 = "Population (total)",
+  "hispanic_native_share"     = "Hispanic native share",
+  "white_share"               = "White share",
+  "native_lt_hs_share"        = "Native <HS share"
+)
+
+sci_fmt <- function(x) {
+  ifelse(
+    is.na(x),
+    "",
+    formatC(x, format = "e", digits = 3)
+  )
+}
+
+modelsummary(
+  two_sls_models,
+  coef_map  = coef_map,
+  # hide controls if you don’t want them shown
+  coef_omit = "pop_total|hispanic_native_share|white_share|native_lt_hs_share",
+  stars     = TRUE,
+  fmt       = sci_fmt,
+  gof_omit  = "IC|Log|Adj|Pseudo",
+  output    = "./output/tables/2sls_results_with24.html",
+  title     = "2SLS Estimates: Immigration Inflows and GOP Two-Party Vote Share (Election Years)"
+)
+
+### YEAR VARYING EFFECTS
+
+panel_fs_pred <- panel_fs %>%
+  mutate(
+    fit_overall = predict(fs_overall, newdata = panel_fs),
+    fit_low_A   = predict(fs_low_A,   newdata = panel_fs),
+    fit_high_A  = predict(fs_high_A,  newdata = panel_fs),
+    fit_low_B   = predict(fs_low_B,   newdata = panel_fs),
+    fit_high_B  = predict(fs_high_B,  newdata = panel_fs)
+  )
+fits_df <- panel_fs_pred %>%
+  select(
+    county_fips,
+    YEAR,
+    fit_overall,
+    fit_low_A,
+    fit_high_A,
+    fit_low_B,
+    fit_high_B
+  ) %>%
+  mutate(
+    county_fips = as.integer(county_fips),
+    YEAR        = as.integer(YEAR)
+  )
+
+panel_iv <- panel_iv %>%
+  mutate(
+    county_fips = as.integer(county_fips),
+    YEAR        = as.integer(YEAR)
+  ) %>%
+  left_join(fits_df, by = c("county_fips", "YEAR"))
+
+panel_iv <- panel_iv %>% select(-YEAR.x, -YEAR.y)
 write.csv(panel_iv, "./data/panel_iv.csv")
+
 
