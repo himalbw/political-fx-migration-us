d6f8c17cf4ee2fc112c82dee15891b7f2c35faf8
himalbw <himalb23@gmail.com>
2025-12-09 19:36:42 -0500

    baseline regression tables

--- code/run_baseline.R
+++ code/run_baseline.R
@@ -1,51 +1,11 @@
-panel_iv <- read_csv("./data/panel_iv.csv")
+df <- read_csv("./data/panel_iv.csv")
 
-m_low_B_fit_year <- feols(
-  gop_two_party_share ~
-    gop_two_party_share_lag4 +
-    pop_total + hispanic_native_share + white_share + native_lt_hs_share +
-    d_fb_low_skill_a_3y |     # <-- year-by-fit interactions here
-    county_fips,                   # FE only for county
-  data    = panel_iv,
-  cluster = ~county_fips
-)
+# scale main vars so they can be per 100k
+df$all <- df$foreign_flow_3y*10000  
+df$low_A <- df$d_fb_low_skill_a_3y*10000
+df$high_A <- df$d_fb_high_skill_a_3y*10000
+df$low_B <- df$d_fb_low_skill_b_3y*10000
+df$high_B <- df$d_fb_high_skill_b_3y*10000
 
-m_total_fit_year <- feols(
-  gop_two_party_share ~
-    gop_two_party_share_lag4 +
-    pop_total + hispanic_native_share + white_share + native_lt_hs_share +
-    fit_overall * factor(YEAR) |
-    county_fips,
-  data    = panel_iv,
-  cluster = ~county_fips
-)
 
-m_low_A_fit_year <- feols(
-  gop_two_party_share ~
-    gop_two_party_share_lag4 +
-    pop_total + hispanic_native_share + white_share + native_lt_hs_share +
-    fit_low_A * factor(YEAR) |
-    county_fips,
-  data    = panel_iv,
-  cluster = ~county_fips
-)
 
-m_high_A_fit_year <- feols(
-  gop_two_party_share ~
-    gop_two_party_share_lag4 +
-    pop_total + hispanic_native_share + white_share + native_lt_hs_share +
-    fit_high_A * factor(YEAR) |
-    county_fips,
-  data    = panel_iv,
-  cluster = ~county_fips
-)
-
-m_high_B_fit_year <- feols(
-  gop_two_party_share ~
-    gop_two_party_share_lag4 +
-    pop_total + hispanic_native_share + white_share + native_lt_hs_share +
-    fit_high_B * factor(YEAR) |
-    county_fips,
-  data    = panel_iv,
-  cluster = ~county_fips
-)
